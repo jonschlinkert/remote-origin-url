5276bd1b0278f9686e8ff78709b6e493cfc967a8
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-25 07:38:19 -0500

    add tests

--- test.js
+++ test.js
@@ -1,11 +1,33 @@
-var expect = require('chai').expect;
-var origin = require('./');
+'use strict';
 
-describe('git remote origin URL:', function () {
+require('should');
+var url = require('./');
+
+describe('async:', function () {
+  it('should trow when not found', function (cb) {
+    url('foo', function (err, res) {
+      err.should.be.an.instanceof(Error);
+      err.message.should.equal('.git/config does not exist');
+      cb();
+    });
+  });
+
+  it('should return the git remote origin URL.', function (cb) {
+    url(function (err, res) {
+      res.should.equal('https://github.com/jonschlinkert/remote-origin-url.git');
+      cb();
+    })
+  });
+});
+
+describe('sync', function () {
   it('should return the git remote origin URL.', function () {
-    var actual = origin.url();
-    console.log('git remote origin url:', actual);
-    var expected = 'https://github.com/jonschlinkert/remote-origin-url.git';
-    expect(actual).to.eql(expected);
+    url.sync().should.equal('https://github.com/jonschlinkert/remote-origin-url.git');
+  });
+
+  it('should throw when config is not found.', function () {
+    (function () {
+      url.sync('foo');
+    }).should.throw('.git/config does not exist.');
   });
 });

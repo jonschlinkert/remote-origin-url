f542883413b86ace8842fec4872ca7f6011bebe3
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-21 04:19:04 -0400

    create temporary `/.git` repo for tests

--- test.js
+++ test.js
@@ -1,31 +1,66 @@
 'use strict';
 
 require('mocha');
-require('should');
+var fs = require('fs');
+var path = require('path');
+var assert = require('assert');
+var gitty = require('gitty');
+var del = require('delete');
 var url = require('./');
+var repo;
 
-describe('async:', function() {
-  it('should return `null` when not found', function(cb) {
-    url('foo', function(err, res) {
-      (res == null).should.be.true;
-      cb();
+var cwd = process.cwd();
+var tmp = path.resolve(__dirname, 'fixtures');
+var git = path.resolve(tmp, '.git');
+
+describe('repo', function() {
+  before(function(cb) {
+    process.chdir(tmp);
+    del(git, function(err) {
+      if (err) return cb(err);
+      repo = gitty(tmp);
+      repo.initSync();
+      repo.addSync(['.']);
+      repo.commitSync('first commit');
+      repo.addRemote('origin', 'https://github.com/jonschlinkert/test-project.git', cb);
     });
   });
 
-  it('should return the git remote origin URL.', function(cb) {
-    url(function(err, res) {
-      res.should.equal('https://github.com/jonschlinkert/remote-origin-url.git');
-      cb();
-    })
+  after(function(cb) {
+    process.chdir(cwd);
+    del(git, cb);
+  });
+
+  describe('async', function() {
+    it('should return the git remote origin URL.', function(cb) {
+      url(function(err, res) {
+        assert.equal(res, 'https://github.com/jonschlinkert/test-project.git');
+        cb();
+      })
+    });
+  });
+
+  describe('sync', function() {
+    it('should return the git remote origin URL.', function() {
+      assert.equal(url.sync(), 'https://github.com/jonschlinkert/test-project.git');
+    });
   });
 });
 
-describe('sync', function() {
-  it('should return the git remote origin URL.', function() {
-    url.sync().should.equal('https://github.com/jonschlinkert/remote-origin-url.git');
+describe('no repo', function() {
+  describe('async', function() {
+    it('should return `null` when not found', function(cb) {
+      url('foo', function(err, res) {
+        if (err) return cb(err);
+        assert.equal(res, null);
+        cb();
+      });
+    });
   });
 
-  it('should return `null` when config is not found.', function() {
-    (url.sync('foo') == null).should.be.true;
+  describe('sync', function() {
+    it('should return `null` when config is not found.', function() {
+      assert.equal(url.sync('foo'), null);
+    });
   });
 });
